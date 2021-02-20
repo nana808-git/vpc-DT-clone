e875a95007403ba63a39ed1d2b0136b90528f038
nana808-git <nana.devtestacct@gmail.com>
2021-02-19 21:24:39 -0600

    ok

--- dns.tf
+++ dns.tf
@@ -1,12 +1,12 @@
-resource "aws_route53_zone" "main" {
-  name          = "${var.app["name"]}-${var.app["env"]}.internal"
-  force_destroy = true
+#resource "aws_route53_zone" "main" {
+#  name          = "${var.app["name"]}-${var.app["env"]}.internal"
+#  force_destroy = true
 
-  vpc {
-    vpc_id = "${aws_vpc.vpc.id}"
-  }
+#  vpc {
+#    vpc_id = "${aws_vpc.vpc.id}"
+#  }
 
-  lifecycle {
-    ignore_changes = ["vpc"]
-  }
-}
+#  lifecycle {
+#    ignore_changes = ["vpc"]
+#  }
+#}
