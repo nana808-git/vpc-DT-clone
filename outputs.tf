72bfb5699a150d930b2cb36025b9f0939df1a8d5
nana808-git <nana.devtestacct@gmail.com>
2021-03-22 09:31:03 -0500

    ok

--- outputs.tf
+++ outputs.tf
@@ -25,3 +25,11 @@ output "public_route_table" {
 output "zone_id" {
   value = "${aws_route53_zone.main.zone_id}"
 }
+
+output "public_subnets" {
+  value = ["${aws_subnet.public.*.cidr_block}"]
+}
+
+output "private_subnets" {
+  value = ["${aws_subnet.private.*.cidr_block}"]
+}  
